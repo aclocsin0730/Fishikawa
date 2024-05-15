16867a2a289472e2e1e1dac6521545ead92b48d6
aclocsin <109121387+aclocsin0730@users.noreply.github.com>
2024-05-15 10:32:12 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def show_images():
     st.title("Reaction Time Test")
     
     # Load images and correct answers from CSV
-    df = pd.read_csv("Fishikawa\images.csv")
+    df = pd.read_csv("images.csv")
     images_set1 = df[:len(df)//2].values.tolist()  
     images_set2 = df[len(df)//2:].values.tolist()  
     

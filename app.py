60558b2ca58f6cb33150679bdd1a5c627292e6d9
aclocsin <109121387+aclocsin0730@users.noreply.github.com>
2024-05-15 10:31:10 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import time
 
 # Google Sheets authentication
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name("Fishikawa/paper-423314-05d2c7e30259.json", scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name("paper-423314-05d2c7e30259.json", scope)
 client = gspread.authorize(creds)
 sheet = client.open("fishikawa_data").sheet1
 
